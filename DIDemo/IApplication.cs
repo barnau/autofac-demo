bb8f38d41aa1ff7058cec192e5e6663e0f103ae1
barnau <blainemarnau@gmail.com>
2018-12-07 16:19:37 -0800

    auto fac demo

---
+++ DIDemo/IApplication.cs
@@ -0,0 +1,7 @@
+﻿namespace DIDemo
+{
+    public interface IApplication
+    {
+        void Run();
+    }
+}
---
+++ DIDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace DIDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = ContainerConfig.Configure();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+
+                app.Run();
+            }
+
+                #region hide
+                //var container = ContainerConfig.Configure();
+                //using (var scope = container.BeginLifetimeScope())
+                //{
+                //    var app = scope.Resolve<IApplication>();
+
+                //    app.Run();
+                //}
+                #endregion
+
+
+                Console.ReadLine();
+        }
+    }
+}
